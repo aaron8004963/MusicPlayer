981780b9790b87a01216172ee63cd605fdc2107b
Aaron Li <51304175+aaron8004963@users.noreply.github.com>
2023-09-13 00:33:45 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,221 @@
+// Define music data for each day
+var musicData = {
+    "2023-09-10": {
+        title: "Music 1",
+        source: "music/singleRomantic.ogg"
+    },
+    "2023-09-15": {
+        title: "Music 2",
+        source: "music/slow-snow.mp3"
+    },
+   /* ... */
+};
+
+(function ($) {
+
+    "use strict";
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var today = new Date(),
+            year = today.getFullYear(),
+            month = today.getMonth(),
+            monthTag = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            day = today.getDate(),
+            days = document.getElementsByTagName('td'),
+            selectedDay,
+            setDate,
+            daysLen = days.length;
+        // options should like '2014-01-01'
+        function Calendar(selector, options) {
+            this.options = options;
+            this.draw();
+        }
+
+        Calendar.prototype.draw = function () {
+            this.getCookie('selected_day');
+            this.getOptions();
+            this.drawDays();
+            var that = this,
+                reset = document.getElementById('reset'),
+                pre = document.getElementsByClassName('pre-button'),
+                next = document.getElementsByClassName('next-button');
+
+            pre[0].addEventListener('click', function () { that.preMonth(); });
+            next[0].addEventListener('click', function () { that.nextMonth(); });
+            reset.addEventListener('click', function () { that.reset(); });
+            while (daysLen--) {
+                days[daysLen].addEventListener('click', function () { that.clickDay(this); });
+            }
+        };
+
+        Calendar.prototype.drawHeader = function (e) {
+            var headDay = document.getElementsByClassName('head-day'),
+                headMonth = document.getElementsByClassName('head-month');
+
+            e ? headDay[0].innerHTML = e : headDay[0].innerHTML = day;
+            headMonth[0].innerHTML = monthTag[month] + " - " + year;
+        };
+
+        Calendar.prototype.drawDays = function () {
+            var startDay = new Date(year, month, 1).getDay(),
+                //      下面表示这个月总共有几天
+                nDays = new Date(year, month + 1, 0).getDate(),
+
+                n = startDay;
+            //      清除原来的样式和日期
+            for (var k = 0; k < 42; k++) {
+                days[k].innerHTML = '';
+                days[k].id = '';
+                days[k].className = '';
+            }
+
+            for (var i = 1; i <= nDays; i++) {
+                days[n].innerHTML = i;
+                n++;
+            }
+
+            for (var j = 0; j < 42; j++) {
+                if (days[j].innerHTML === "") {
+
+                    days[j].id = "disabled";
+
+                } else if (j === day + startDay - 1) {
+                    if ((this.options && (month === setDate.getMonth()) && (year === setDate.getFullYear())) || (!this.options && (month === today.getMonth()) && (year === today.getFullYear()))) {
+                        this.drawHeader(day);
+                        days[j].id = "today";
+                    }
+                }
+                if (selectedDay) {
+                    if ((j === selectedDay.getDate() + startDay - 1) && (month === selectedDay.getMonth()) && (year === selectedDay.getFullYear())) {
+                        days[j].className = "selected";
+                        this.drawHeader(selectedDay.getDate());
+                    }
+                }
+            }
+        };
+
+        Calendar.prototype.clickDay = function (o) {
+            var selected = document.getElementsByClassName("selected"),
+                len = selected.length;
+            if (len !== 0) {
+                selected[0].className = "";
+            }
+            o.className = "selected";
+            selectedDay = new Date(year, month, o.innerHTML);
+            this.drawHeader(o.innerHTML);
+            this.setCookie('selected_day', 1);
+
+        };
+
+        Calendar.prototype.preMonth = function () {
+            if (month < 1) {
+                month = 11;
+                year = year - 1;
+            } else {
+                month = month - 1;
+            }
+            this.drawHeader(1);
+            this.drawDays();
+        };
+
+        Calendar.prototype.nextMonth = function () {
+            if (month >= 11) {
+                month = 0;
+                year = year + 1;
+            } else {
+                month = month + 1;
+            }
+            this.drawHeader(1);
+            this.drawDays();
+        };
+
+        Calendar.prototype.getOptions = function () {
+            if (this.options) {
+                var sets = this.options.split('-');
+                setDate = new Date(sets[0], sets[1] - 1, sets[2]);
+                day = setDate.getDate();
+                year = setDate.getFullYear();
+                month = setDate.getMonth();
+            }
+        };
+
+        Calendar.prototype.reset = function () {
+            month = today.getMonth();
+            year = today.getFullYear();
+            day = today.getDate();
+            this.options = undefined;
+            this.drawDays();
+        };
+
+        Calendar.prototype.setCookie = function (name, expiredays) {
+            if (expiredays) {
+                var date = new Date();
+                date.setTime(date.getTime() + (expiredays * 24 * 60 * 60 * 1000));
+                var expires = "; expires=" + date.toGMTString();
+            } else {
+                var expires = "";
+            }
+            document.cookie = name + "=" + selectedDay + expires + "; path=/";
+        };
+
+        Calendar.prototype.getCookie = function (name) {
+            if (document.cookie.length) {
+                var arrCookie = document.cookie.split(';'),
+                    nameEQ = name + "=";
+                for (var i = 0, cLen = arrCookie.length; i < cLen; i++) {
+                    var c = arrCookie[i];
+                    while (c.charAt(0) == ' ') {
+                        c = c.substring(1, c.length);
+
+                    }
+                    if (c.indexOf(nameEQ) === 0) {
+                        selectedDay = new Date(c.substring(nameEQ.length, c.length));
+                    }
+                }
+            }
+        };
+        var calendar = new Calendar();
+
+        //*********************************************************************************************************/
+
+        // Function to update the music details when a day is clicked
+        // Function to update the music details when a day is clicked
+        function updateMusicDetails(date) {
+            var selectedDate = date;
+            var musicInfo = musicData[selectedDate];
+            var musicDetails = document.getElementById("music-details");
+
+            if (musicInfo) {
+                musicDetails.innerHTML = `
+            <h3>${musicInfo.title}</h3>
+            <button class="play-button" data-source="${musicInfo.source}">Play</button>
+        `;
+                // Add an event listener to the play button
+                var playButton = musicDetails.querySelector('.play-button');
+                playButton.addEventListener('click', function () {
+                    // Post a message to musicPlayer.html with the music source
+                    window.parent.postMessage({ type: 'changeMusic', source: musicInfo.source }, '*');
+                });
+
+            } else {
+                musicDetails.innerHTML = '<p>No music available for this day.</p>';
+            }
+        }
+
+
+
+        // Add click event listeners to calendar days
+        var calendarDays = document.querySelectorAll('#calendar tbody td');
+        calendarDays.forEach(function (day) {
+            day.addEventListener('click', function () {
+                // Convert the clicked date to "YYYY-MM-DD" format
+                var clickedDate = year + '-' + (month + 1).toString().padStart(2, '0') + '-' + this.innerHTML;
+                updateMusicDetails(clickedDate);
+            });
+        });
+
+    }, false);
+
+})(jQuery);
+
+//*********************************************************************************************************/
---
+++ js/script.js
@@ -0,0 +1,198 @@
+//*********************************************************************************************************/
+
+//navbar collapse close
+// Add a click event listener to the navbar toggler
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all the navbar links
+    var navbarLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+    // Add click event listeners to the links
+    navbarLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            // Check if the navigation menu is collapsed
+            if (window.innerWidth <= 992) {
+                // If it's collapsed, close it
+                var navbarToggler = document.querySelector(".navbar-toggler");
+                if (navbarToggler.classList.contains("collapsed")) {
+                    // Check if the navbar is collapsed, if not, don't close it
+                    return;
+                }
+                navbarToggler.click(); // Close the navbar
+            }
+
+            // Smooth scroll to the clicked section
+            e.preventDefault();
+            var targetId = link.getAttribute("href").substring(1); // Get the target section ID
+            var targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
+
+//close the navbar when window become large
+// Function to close the Bootstrap 4 navbar collapse when the window is resized to medium or larger
+function closeNavbarOnResize() {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+
+    // Check if the window width is medium or larger (992 pixels or more)
+    if (window.innerWidth >= 992 && !navbarToggler.classList.contains("collapsed")) {
+        // Check if the navbar collapse is open
+        if (navbarCollapse.classList.contains("show")) {
+            navbarToggler.click(); // Close the navbar collapse
+        }
+    }
+}
+
+// Add a resize event listener to close the navbar collapse on window resize
+window.addEventListener('resize', closeNavbarOnResize);
+
+
+//*********************************************************************************************************/
+
+// Get the h1 element
+const h1 = document.querySelector('h1');
+
+// Function to increase font size and letter spacing on hover
+function applyHoverEffect(event) {
+    const letter = event.target;
+    const isWhitespace = /\s/.test(letter.textContent); // Check if it's a whitespace character
+    if (!isWhitespace) {
+        letter.style.transition = 'font-size 0.2s ease-in-out'; // Shorter transition duration
+        const originalSize = getComputedStyle(letter).fontSize;
+        const newSize = `calc(${originalSize} + ${parseFloat(originalSize) * 0.8}px)`; // Increase the font size by 12%
+        letter.style.fontSize = newSize;
+        letter.style.letterSpacing = '-0.05em'; // Adjust the letter spacing to prevent spacing increase
+    }
+}
+
+// Function to reset font size and letter spacing on mouseout
+function resetHoverEffect(event) {
+    const letter = event.target;
+    letter.style.fontSize = ''; // Reset font size
+    letter.style.letterSpacing = ''; // Reset letter spacing
+}
+
+// Wrap each letter in a span element and apply the hover effect
+const text = h1.textContent;
+const letters = text.split('');
+const modifiedText = letters.map(letter => {
+    const span = document.createElement('span');
+    span.textContent = letter;
+    span.addEventListener('mouseover', applyHoverEffect);
+    span.addEventListener('mouseout', resetHoverEffect);
+    return span;
+});
+
+// Replace the h1 content with the modified text
+h1.textContent = '';
+modifiedText.forEach(span => {
+    h1.appendChild(span);
+});
+
+//*********************************************************************************************************/
+
+//Lycris button
+document.addEventListener("DOMContentLoaded", function () {
+    const lyricsControl = document.getElementById("lyrics-button");
+
+    // Function to toggle sound
+    function togglelyrics() {
+        if (lyricsControl.textContent == "Lyrics: Off") {
+            lyricsControl.textContent = "Lyrics: On";
+        } else {
+            lyricsControl.textContent = "Lyrics: Off";
+        }
+    }
+
+    // Add click event listener to sound control button
+    lyricsControl.addEventListener("click", togglelyrics);
+});
+
+
+//*********************************************************************************************************/
+
+//Sound button
+document.addEventListener("DOMContentLoaded", function () {
+    const bgMusic = document.getElementById("bg-music");
+    const soundControl = document.getElementById("sound-control");
+
+    // Function to toggle sound
+    function toggleSound() {
+        if (bgMusic.paused) {
+            bgMusic.play();
+            soundControl.textContent = "Sound: On";
+        } else {
+            bgMusic.pause();
+            soundControl.textContent = "Sound: Off";
+        }
+    }
+
+    // Add click event listener to sound control button
+    soundControl.addEventListener("click", toggleSound);
+});
+
+//*********************************************************************************************************/
+// Listen for messages from calendar and paly music if sound on
+window.addEventListener('message', function (event) {
+    if (event.data.type === 'changeMusic') {
+        // Change the background music source
+        var bgMusic = document.getElementById('bg-music');
+        bgMusic.src = event.data.source;
+        bgMusic.load();
+        // Check if sound is enabled
+        var soundControl = document.getElementById('sound-control');
+        var isSoundEnabled = soundControl.textContent.includes('Sound: On');
+        if (isSoundEnabled) {
+            bgMusic.play();
+        }else{
+            bgMusic.pause();
+        }
+    }
+});
+
+//*********************************************************************************************************/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hero = document.getElementById("hero-container");
+    const navbarHeight = document.querySelector(".navbar").offsetHeight+200;
+
+    window.addEventListener("scroll", function() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+        // Calculate the opacity based on the scroll position
+        const opacity = 1 - (scrollTop / navbarHeight);
+
+        // Apply the opacity to the hero section
+        hero.style.opacity = opacity < 0 ? 0 : opacity;
+    });
+});
+//*********************************************************************************************************/
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the arrow element
+    const scrollArrow = document.getElementById('scroll-arrow');
+
+    // Get the rest section
+    const restSection = document.getElementById('musicPlayer-container');
+
+    // Add a click event listener to the arrow for smooth scrolling
+    scrollArrow.addEventListener('click', function(event) {
+        event.preventDefault();
+        const offsetTop = restSection.offsetTop;
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth',
+        });
+    });
+
+    // Add a scroll event listener to toggle arrow visibility
+    window.addEventListener('scroll', toggleArrowVisibility);
+
+    // Initially hide the arrow when the page loads
+    toggleArrowVisibility();
+});
